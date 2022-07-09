d689e27322524be3fe25c661245d260b6fe30c69
qenawi <ahmed@qenawi>
2022-07-09 22:09:41 +0200

    switched to firefox engine

--- linked_in_bot/scrape_person.py
+++ linked_in_bot/scrape_person.py
@@ -1,9 +1,14 @@
 import os
+from selenium.webdriver.firefox.options import Options
 from linkedin_scraper import Person,actions
 from selenium import webdriver
-driver = webdriver.Chrome("./chromedriver")
 
-email = os.getenv("LINKEDIN_USER")
-password = os.getenv("LINKEDIN_PASSWORD")
+options = Options()
+options.binary_location = "/home/ahmedqenawi/Downloads/firefox/firefox"
+
+driver = webdriver.Firefox(executable_path="/home/ahmedqenawi/linked_in_profiler/geckodriver" , options= options)
+email = os.getenv("ahmu@kl.com")
+password = os.getenv("123123")
 actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
 person = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5", driver=driver)
+print(person.experiences)
